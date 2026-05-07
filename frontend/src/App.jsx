86e10c8e9841db112415afc26ba8c7cd410bf437
Avni <avnieesinghall@gmail.com>
2026-05-08 00:50:22 +0530

    Fix CreatePost route import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar";
 
-import Home from "./pages/Home.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
-import CreatePost from "./pages/CreatePost.jsx";
-import Profile from "./pages/Profile.jsx";
-import SinglePost from "./pages/SinglePost.jsx";
-import EditPost from "./pages/EditPost.jsx";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import CreatePost from "./pages/CreatePost";
+import Profile from "./pages/Profile";
+import SinglePost from "./pages/SinglePost";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="app">
-        <Navbar />
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/write" element={<CreatePost />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/post/:id" element={<SinglePost />} />
-          <Route path="/edit/:id" element={<EditPost />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/write" element={<CreatePost />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/post/:id" element={<SinglePost />} />
+      </Routes>
     </BrowserRouter>
   );
 }
